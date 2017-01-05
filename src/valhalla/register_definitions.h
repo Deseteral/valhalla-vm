5607d9a1faae7ebba4828461c651517007be2c5e
Michał Ferchow <deseteral@gmail.com>
2017-01-05 21:25:01 +0100

    Initial fibonacci

--- src/valhalla/register_definitions.h
+++ src/valhalla/register_definitions.h
@@ -8,15 +8,19 @@ struct RegisterDefinition
     u8 byte;
 };
 
-#define REGISTER_COUNT 6
 static RegisterDefinition registerDefinitions[] = {
     RegisterDefinition{ "X", 0x0 },
     RegisterDefinition{ "Y", 0x1 },
     RegisterDefinition{ "C", 0x2 },
     RegisterDefinition{ "W", 0x3 },
     RegisterDefinition{ "A", 0x4 },
-    RegisterDefinition{ "B", 0x5 }
+    RegisterDefinition{ "B", 0x5 },
+    RegisterDefinition{ "J", 0x6 },
+    RegisterDefinition{ "K", 0x7 },
+    RegisterDefinition{ "L", 0x8 },
+    // update REGISTER_COUNT!
 };
+#define REGISTER_COUNT 9
 
 static bool isTokenRegister(string token)
 {
